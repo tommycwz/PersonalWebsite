1489215e9794426450df80d3a32429f271ab563b
tommycwz <tommycwz2@yahoo.com>
2021-05-15 04:49:14 +0000

    test

--- Scripts/app.js
+++
@@ -1,9 +0,0 @@
-// Splash Screen
-const splash = document.querySelector('.splash');
-
-splash.onanimationend = () => {
-    console.log('Animation ended');
-    setTimeout(()=>{
-        splash.classList.add('display-none');
-    },100);
-};
